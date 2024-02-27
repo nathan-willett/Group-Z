9f0d8230611f7f4c2051f1e3fd0108c94e07083e
winnsta <158104929+winnsta@users.noreply.github.com>
2024-02-26 21:24:17 -0800

    Update Item.java

--- app/src/main/java/Item.java
+++ app/src/main/java/Item.java
@@ -25,7 +25,18 @@ public class Item {
     }
 
     public double priceFor(int quantity) {
-        return this.price * quantity;
+        try { // Try/catch for divison by zero
+        if ((quantity % this.bulk_quantity == 0) && this.bulk_quantity > 0) // If the quantity is a multiple of the bulk amount, return the multiple times the bulk price
+            return quantity / this.bulk_quantity * this.bulk_price;
+        else if (this.bulk_quantity > 0) // Returns the calculated price if the quantity is less than the bulk quantity or if it's not a multiple 
+            return (quantity / this.bulk_quantity * this.bulk_price) + (quantity % this.bulk_quantity * this.price);
+        else // If the bulk quantity is zero, return the quantity times the price
+            return quantity*this.price;
+        }
+        catch (ArithmeticException e) {
+            System.out.print("Cannot divide by zero.\n");
+            return 0.0;
+        }
     }
 
     public String toString() {
