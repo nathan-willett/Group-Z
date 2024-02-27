8411e2b9045f45ad43fa2ad7d5982d25157978eb
Nathan Willett <natewillett1996@gmail.com>
2024-02-26 22:01:35 -0800

    rename inventoryList to catalogList

--- app/src/main/java/Catalog.java
+++ app/src/main/java/Catalog.java
@@ -1,7 +1,7 @@
 /**
  * Represents a catalog that stores items.
- * A catalog has a name and a predefined size which limits the number of items
- * it can hold.
+ * 
+ * A catalog has a name and a predefined size which limits the number of items it can hold.
  */
 
 import java.util.ArrayList;
@@ -9,7 +9,7 @@ import java.util.ArrayList;
 public class Catalog {
     String name; // The name of the catalog
     int size; // The maximum number of items the catalog can hold
-    ArrayList<Item> inventoryList = new ArrayList<>(); // A list to store the items in the catalog
+    ArrayList<Item> catalogList = new ArrayList<>(); // A list to store the items in the catalog
 
     /**
      * Constructs a new Catalog instance with the specified name and size.
@@ -29,10 +29,10 @@ public class Catalog {
      * @return true if the item was successfully added, false if the catalog is full
      */
     public boolean add(Item item) {
-        if (this.inventoryList.size() >= this.size) {
+        if (this.catalogList.size() >= this.size) {
             return false; // Catalog is full
         }
-        this.inventoryList.add(item);
+        this.catalogList.add(item); // Otherwise, add the item
         return true;
     }
 
@@ -42,7 +42,7 @@ public class Catalog {
      * @return the size of the catalog
      */
     public int size() {
-        return this.inventoryList.size();
+        return this.catalogList.size();
     }
 
     /**
@@ -52,7 +52,7 @@ public class Catalog {
      * @return the item at the specified index
      */
     public Item get(int index) {
-        return inventoryList.get(index);
+        return catalogList.get(index);
     }
 
     /**
