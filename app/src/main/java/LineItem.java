e1d5c13444c587287fc291511e01feb335881574
Nathan Willett <natewillett1996@gmail.com>
2024-03-10 18:32:50 -0700

    added exception handling to LineItem

--- app/src/main/java/LineItem.java
+++ app/src/main/java/LineItem.java
@@ -3,17 +3,29 @@
  * This class is used to track the details of items ordered by a customer.
  */
 public class LineItem {
-    Item item; // The item being ordered
-    int quantity; // The quantity of the item ordered
+    private final Item item; // The item being ordered
+    private final int quantity; // The quantity of the item ordered
 
     /**
-     * Constructs an ItemOrder with the specified item and quantity.
+     * Constructs a LineItem with the specified item and quantity.
      *
      * @param item The item being ordered
      * @param quantity The quantity of the item being ordered
+     * @throws IllegalArgumentException if item is null or quantity is less than 1
      */
     public LineItem(Item item, int quantity) {
+        if (item == null || quantity < 1) {
+            throw new IllegalArgumentException("Item cannot be null and quantity must be at least 1.");
+        }
         this.item = item;
         this.quantity = quantity;
+    }
+
+    public Item getItem() {
+        return item;
+    }
+
+    public int getQuantity() {
+        return quantity;
     }
 }
